02ff385e5898ac0819c218687c3047588e6cdb15
João Inácio Neto <birobirobiro.dev@gmail.com>
2021-03-30 23:37:10 -0300

    :tada: Aula 02 finalizada

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 
-//usando template engine
-server.set('view engine', 'ejs');
+// usando template engine
+server.set('view engine',  'ejs')
 
 //habilitar arquivos statics
 server.use(express.static("public"))
+
+// usar o req.body
+server.use(express.urlencoded({ extended: true }))
 
 // routes
 server.use(routes)
